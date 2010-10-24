46c4f7833e6bca74ceaefdf8ea433996d1e4e492
DecoDe <code723@gmail.com>
2010-10-24 21:57:52 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trade::Application.config.secret_token = '5341b3b2339686e121d379275eab110af7073ddf479eb4eaa065be76f067917b12e01550ab9369fc16f9cd4d57fcce29af8e3fa067d9ebdc78dd6ccf02b05aea'
